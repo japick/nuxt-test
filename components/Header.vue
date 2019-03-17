ffff8d073f65bed35e6333f6c888c2a9350065d9
Jay Pick <jay@madebysix.com>
2019-03-17 09:10:10 +0000

    [dev] contentful preview api

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,46 @@
 <template>
-  <div>
-    <!-- <ul>
+  <div class="c-Header">
+    <nuxt-link :to="'/'">Home</nuxt-link>
+    <ul>
       <li v-for="(page, index) in pages" :key="index">
-        <nuxt-link :to="page.fields.slug">{{page.fields.title}}</nuxt-link>
-        {{page.fields.title}}
+        <nuxt-link :to="'/' + page.fields.slug">{{page.fields.title}}</nuxt-link>
       </li>
-    </ul> -->
+    </ul>
   </div>
 </template>
 
 <script>
+import client from '~/plugins/contentful'
 
+export default {
+  data() {
+    return {
+      pages: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      return client.getEntries({
+        content_type: 'page'
+      }).then((entries) => {
+        this.pages = entries.items
+      }).catch(e => {
+        console.log(e)
+      })
+    }
+  }
+}
 </script>
+
+<style>
+.c-Header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+</style>
--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,8 +1,15 @@
 const contentful = require('contentful')
-
-const client = contentful.createClient({
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_ACCESS_TOKEN
-})
+const defaultConfig = {
+	host: 'preview.contentful.com',
+	space: process.env.CTF_SPACE_ID,
+	accessToken: process.env.CTF_CPA_TOKEN
+}
+const client = contentful.createClient(defaultConfig)
 
 module.exports = client
+
+// production env using Content Delivery API
+/* if (process.env.NODE_ENV === 'production') {
+	options.host = 'cdn.contentful.com',
+	options.accessToken = config.CTF_CDA_TOKEN
+} */
